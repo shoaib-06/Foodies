01837df0187ee6e3c8410f97a6ef6a818fc1a02e
shoaib-06 <shoaiblatif090@gmail.com>
2023-03-13 18:04:12 +0500

    Added Faq and Testimonial

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 console.log("Hello World!");
+
+const swiper = new Swiper('.swiper', {
+  speed: 400,
+  spaceBetween: 50,
+
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: "true",
+  },
+});
