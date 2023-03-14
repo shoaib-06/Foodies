161c0febf3b2eca21355b633caf1e278f8c4e5d7
shoaib-06 <shoaiblatif090@gmail.com>
2023-03-14 12:48:40 +0500

    Added Button

--- app.js
+++ app.js
@@ -44,3 +44,12 @@ window.onscroll = function () {
     nav.classList.remove("scroll-on");
   }
 }
+
+// < !-- Close Nav -->
+let navBar = document.querySelectorAll('.nav-link');
+let navCollapse = document.querySelector('.navbar-collapse.collapse');
+navBar.forEach(function (a) {
+  a.addEventListener("click", function () {
+    navCollapse.classList.remove("show");
+  })
+})
